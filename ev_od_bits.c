cd82dfa38302032b808333de7d91341c7f659fd8
Your Name <you@example.com>
2023-05-04 12:19:14 +0530

    04052023

---
+++ ev_od_bits.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int edd(int a)
+{
+int i;
+for(i=1;i<4;i++)
+{
+if(i%2==0){
+
+
+}
+}
+}
+
+int main()
+{
+int n,i;
+printf("Enter n value");
+scanf("%d",&n);
+
+int (*fp)(int);
+fp=edd;
+fp(n);
+}
---
+++ extract_bits.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+
+int a=15;
+a= a >>2;
+int c = ((1<<6)-1 );
+printf("%d",a &c );
+}
+
